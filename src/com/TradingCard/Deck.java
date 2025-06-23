eb47dc7c72d604e162ee9b68c119eb7a9375980d
Helyks02 <110266628+Helyks02@users.noreply.github.com>
2025-06-23 18:11:33 +0800

    Added attributes, constructor, and viewDeck method skeleton

--- src/com/TradingCard/Deck.java
+++ src/com/TradingCard/Deck.java
@@ -1,4 +1,14 @@
 package com.TradingCard;
 
 public class Deck {
+  private Card[] cards;
+
+  public Deck {
+  cards = new Card[10];
+  }
+
+  public void viewDeck(){
+    for (int i = 0; int < 10; i++){
+    
+    }
 }
