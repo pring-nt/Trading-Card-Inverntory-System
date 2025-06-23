7fdfb9ed83c6ba9a84d581a5ccec338b130a1b1f
Helyks02 <110266628+Helyks02@users.noreply.github.com>
2025-06-23 20:11:21 +0800

    changed indentations

--- src/com/TradingCard/Binder.java
+++ src/com/TradingCard/Binder.java
@@ -1,17 +1,17 @@
 package com.TradingCard;
 
 public class Binder {
-  private String name;
-  private Card[] cards;
+    private String name;
+    private Card[] cards;
+    
+    public Binder (){
+        name = "";
+        cards = new Card[20];
+    }
 
-  public Binder (){
-    name = "";
-    cards = new Card[20];
-  }
-
-  public void viewBinder(){
-    System.out.println("Binder Name: " + this.name);
-    for (int i = 0; i < 20; i++){
+    public void viewBinder(){
+        System.out.println("Binder Name: " + this.name);
+        for (int i = 0; i < 20; i++){
       
     }
 }
