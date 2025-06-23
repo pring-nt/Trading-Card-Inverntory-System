ac97af7442fb3d022f420ce6066a751653cba09c
nathan <natetrndd@gmail.com>
2025-06-23 13:51:23 +0800

    Modified the card constructor, added getters, modified Variation.java to just have the variants and no multipliers

--- src/com/TradingCard/Card.java
+++ src/com/TradingCard/Card.java
@@ -1,17 +1,56 @@
 package com.TradingCard;
 
+import java.math.BigDecimal;
+
 public class Card {
     private final String name;
     private final Rarity rarity;
     private final Variation variation;
-    private double value;
+    private final BigDecimal value;
     private int count;
 
-    public Card(String n, Rarity r, Variation v, double val) {
+    public Card(String n, Rarity r, Variation v, BigDecimal val) {
         this.name = n;
         this. rarity = r;
         this.variation = v;
-        this.value = val;
+        BigDecimal multiplier;
+        switch(variation){
+            case ALT_ART -> {
+                multiplier = BigDecimal.valueOf(1.5);
+                this.value = multiplier.multiply(val);
+            }
+            case FULL_ART -> {
+                multiplier = BigDecimal.valueOf(2.0);
+                this.value = multiplier.multiply(val);
+            }
+            case EXTENDED_ART -> {
+                multiplier = BigDecimal.valueOf(3.0);
+                this.value = multiplier.multiply(val);
+            }
+            default -> {
+                this.value = val;
+            }
+        }
         count = 1;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public BigDecimal getValue() {
+        return this.value;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public Rarity getRarity() {
+        return this.rarity;
+    }
+
+    public Variation getVariation() {
+        return this.variation;
     }
 }
--- src/com/TradingCard/Variation.java
+++ src/com/TradingCard/Variation.java
@@ -1,19 +1,9 @@
 package com.TradingCard;
 
 public enum Variation {
-    STANDARD(1.0), // For cards that are not rare or legendary
-    NORMAL(1.0),
-    EXTENDED_ART(1.5),
-    FULL_ART(2.0),
-    ALT_ART(3.0);
-
-    private final double multiplier;
-
-    private Variation(double multiplier) {
-        this.multiplier = multiplier;
-    }
-
-    public double getMultiplier() {
-        return multiplier;
-    }
+    STANDARD, // For cards that are not rare or legendary
+    NORMAL,
+    EXTENDED_ART,
+    FULL_ART,
+    ALT_ART;
 }
